382991c1b26283218b3b0a8118aa3c08ef7bf67f
Jaagat Prashar <121129693+Jaagat-P@users.noreply.github.com>
2025-03-04 16:40:59 -0800

    Update sesame.py
    added device verification

--- sesame/sesame.py
+++ sesame/sesame.py
@@ -6,6 +6,12 @@ import soundfile as sf
 import sounddevice as sd
 import io
 
+# Device verification
+devices = sd.query_devices()
+if not any (d['name'] == 'BlackHole 2ch'
+for d in devices):
+  raise RuntimeError("Blackhole 2ch device not found")
+
 prompt = "Pretend like you are reading a harry potter book and start reading the first chapter."
 
 # Play audio through BlackHole 2ch
